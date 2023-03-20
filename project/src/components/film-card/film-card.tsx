fc3eb8c275a4ae0c747a82189c5b164cf3575bd4
Godskitchen <diemygod@gmail.com>
2023-03-20 18:08:30 +0300

    добавлен state компоненту FilmList

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom';
+import { MouseEvent } from 'react';
 
 type FilmCardProps = {
   id: number;
   name: string;
   previewImage: string;
+  onCardEnter(evt : MouseEvent<HTMLElement>) : void;
+  onCardLeave(): void;
 }
 
-export default function FilmCard({id, name, previewImage} : FilmCardProps) : JSX.Element {
+export default function FilmCard({id, name, previewImage, onCardEnter, onCardLeave} : FilmCardProps) : JSX.Element {
+
+  const handleCardMouseEnter = (evt : MouseEvent<HTMLElement>) => onCardEnter(evt);
+
+  const handleCardMouseLeave = () => onCardLeave();
 
   return (
-    <article className="small-film-card catalog__films-card" onMouseOver={() => null}>
+    <article className="small-film-card catalog__films-card" data-id={id} onMouseEnter={handleCardMouseEnter} onMouseLeave={handleCardMouseLeave}>
       <Link className="small-film-card__link" to={`films/${id}`}>
         <div className="small-film-card__image">
           <img src={previewImage} alt={name} width="280" height="175" />
--- project/src/components/film-list/film-list.tsx
+++ project/src/components/film-list/film-list.tsx
@@ -1,5 +1,6 @@
-//import { useState } from 'react';
+import { useState } from 'react';
 import { Film, Films } from '../../types/film';
+import { MouseEvent } from 'react';
 import FilmCard from '../film-card/film-card';
 
 type FilmListProps = {
@@ -8,10 +9,34 @@ type FilmListProps = {
 
 export default function FilmList({filmList} : FilmListProps) : JSX.Element {
 
+  const [activeCardId, setActiveCardId] = useState('');
+
+  const onCardEnter = (evt : MouseEvent<HTMLElement>) => {
+    if (evt.currentTarget.dataset['id']) {
+      setActiveCardId(evt.currentTarget.dataset['id']);
+    }
+  };
+
+  const onCardLeave = () => {
+    setActiveCardId('');
+  };
+
+  // eslint-disable-next-line no-console
+  console.log('activeCardid = ', activeCardId);
+
   return (
     <div className="catalog__films-list">
       {filmList.map(({id, name, previewImage} : Film) =>
-        (<FilmCard key={`${id}`} id={id} name={name} previewImage={previewImage} />)
+        (
+          <FilmCard
+            key={`${id}`}
+            id={id}
+            name={name}
+            previewImage={previewImage}
+            onCardEnter={onCardEnter}
+            onCardLeave={onCardLeave}
+          />
+        )
       )}
     </div>
   );
